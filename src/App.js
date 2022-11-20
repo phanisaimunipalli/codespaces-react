7e3244ff725c89640b6ed266064b86206027d25d
Phani Sai Ram Munipalli <34421500+phanisaimunipalli@users.noreply.github.com>
2022-11-20 22:59:28 +0000

    First Change of Avatar

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+        {/* <img src="Octocat.png" className="App-logo" alt="logo" /> */}
+        <img width="20%" src='https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortWaved&accessoriesType=Prescription02&hairColor=Black&facialHairType=BeardLight&facialHairColor=Black&clotheType=Hoodie&clotheColor=Black&eyeType=Default&eyebrowType=Default&mouthType=Smile&skinColor=Light'/>
         <p>
           GitHub Codespaces <span className="heart">♥️</span> React
         </p>
-        <p className="small">
+        {/* <p className="small">
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
         <p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.linkedin.com/in/iamphanisairam/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Phani Sai Ram Munipalli
           </a>
         </p>
       </header>
